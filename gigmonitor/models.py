6cbc44144367889589cf2784dbcbaaa7c69ccdf5
Oleksiy Litvinov <leetwinoff92@gmail.com>
2023-11-13 22:40:08 -0600

    Created Artist model with str method

--- gigmonitor/models.py
+++ gigmonitor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    spotify_id = models.CharField(max_length=200, unique=True, null=True, blank=True)
+    soundcloud_id = models.CharField(max_length=200, unique=True, null=True)
+    resident_advisor_id = models.CharField(max_length=200, unique=True, null=True)
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
